09b4264fb86465c3c602c5032727f60ff801ec96
Luli <giulibucci16@gmail.com>
2025-10-27 22:08:08 -0300

    Primer commit - Panel de Misiones

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// Archivo JS principal
+console.log("JS funcionando correctamente ✅");
+
+// Ejemplo: mostrar un mensaje al cargar
+window.addEventListener('DOMContentLoaded', () => {
+  console.log("Página lista para usar");
+});
